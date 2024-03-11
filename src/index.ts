983c699f4debdfa779a770048c188588e71dd0b1
Chris Zimmermann <chris.zimmermann@hotmail.ch>
2024-03-11 07:55:10 +0100

    small changes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import http from "http"
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
@@ -6,8 +6,18 @@ import compression from 'compression'
 import cors from 'cors'
 
 
+const app = express();
+const port = 3000; // You can change this to any port number you prefer
 
+// Middleware to parse JSON bodies
+app.use(express.json());
 
+// Basic route to test the server
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, woooold!');
+});
 
-
-console.log("hello node")
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
